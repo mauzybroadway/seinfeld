6a48db4015a5cb36d16311ecec006392a82f3892
davidalim <14.d.lim@gmail.com>
2016-02-01 15:01:36 -0500

    Sound from SD card.

---
+++ seinfeld.ino
@@ -0,0 +1,45 @@
+#include <SD.h>                      // need to include the SD library
+//#define SD_ChipSelectPin 53  //example uses hardware SS pin 53 on Mega2560
+#define SD_ChipSelectPin 10  //using digital pin 4 on arduino nano 328
+#include <TMRpcm.h>           //  also need to include this library...
+#include "pitches.h"
+
+TMRpcm tmrpcm;   // create an object for use in this sketch
+char mychar;
+
+void setup(){
+
+  tmrpcm.speakerPin = 9; //11 on Mega, 9 on Uno, Nano, etc
+  Serial.begin(9600);
+  
+  if (!SD.begin(SD_ChipSelectPin)) {  // see if the card is present and can be initialized:
+    Serial.println("SD fail");  
+    return;   // don't do anything more if not
+  }
+  Serial.println("helllo");
+  tmrpcm.play("1.wav"); //the sound file "music" will play each time the arduino powers up, or is reset
+
+}
+
+void loop(){  
+  
+  if(Serial.available()){   
+    mychar = Serial.read();
+    if(mychar == 'o'){ //send the letter p over the serial monitor to start playback
+      tmrpcm.play("cd.wav");
+    } 
+    else if(mychar == '0'){ //send the letter p over the serial monitor to start playback
+      tmrpcm.play("0.wav");
+    }
+    else if(mychar == '1'){ //send the letter p over the serial monitor to start playback
+      tmrpcm.play("1.wav");
+    }
+        else if(mychar == 'a'){ //send the letter p over the serial monitor to start playback
+      tmrpcm.play("10.wav");
+    }
+
+  }
+
+}
+
+
